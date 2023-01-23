13c0fe17df956f588b7a09c426e0afaa75c6bbf4
Dmitriy Sikorskiy <sikorskiy_1997@list.ru>
2023-01-23 11:59:40 +0300

    fixed

--- src/main.java
+++ src/main.java
@@ -1,17 +1,16 @@
 public class main {
     public static void main(String[] args) {
-            int score =100;
-            int depositAccount =1100;
-            int bonus;
+        int score = 100;
+        int depositAccount = 1100;
+        int bonus;
 
-            if (depositAccount > 1000) {
-                bonus = depositAccount / 100;
-            }
-            else {
-                bonus = 0;
-            }
+        if (depositAccount > 1000) {
+            bonus = depositAccount / 100;
+        } else {
+            bonus = 0;
+        }
 
-            int balance = score + depositAccount + bonus;
-            System.out.println("Ваш баланс: " + balance + " руб. Ваш бонус: " + bonus + " руб.");
+        int balance = score + depositAccount + bonus;
+        System.out.println("Ваш баланс: " + balance + " руб. Ваш бонус: " + bonus + " руб.");
     }
 }
